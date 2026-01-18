83763d6aa6c741c2ab3f01128fab6db993943d39
Kapil <kydv901@gmail.com>
2026-01-18 17:26:49 +0530

    Create 291A-Bear-and-Big-Brother.cpp

---
+++ 291A-Bear-and-Big-Brother.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    
+    int count = 0;
+    while(a <= b){
+        a = a*3;
+        b = b*2;
+        count++;
+    }
+    
+    cout<<count<<endl;
+    return 0;
+}
