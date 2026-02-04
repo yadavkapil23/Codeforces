a2f9c45954602d228931e727d305d4c21e13ac2e
Kapil <kydv901@gmail.com>
2026-02-04 16:58:39 +0530

    Add logic for counting based on max and min positions
    Implement logic to find the maximum and minimum values in a vector and calculate the count based on their positions.

---
+++ 144A-Arrival-of-the-General.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int> v;
+    int a;
+    for(int i=0 ; i<n ; i++){
+        cin>>a;
+        v.push_back(a);
+    }
+    
+    
+    int count = 0;
+    int maxin;
+    int minin;
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+    
+    for(int i=0; i<n ; i++){
+        if(v[0] == maxi){
+            count = 0;
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        if(v[i] > maxi){
+            maxi = v[i];
+            maxin = i;
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        if(v[i] <= mini){
+            mini = v[i];
+            minin = i;
+        }
+    }
+        
+    count = maxin + (n-1-minin);
+    
+    if (maxin > minin) count--;
+
+    
+    cout<<count<<endl;
+    return 0;
+}
