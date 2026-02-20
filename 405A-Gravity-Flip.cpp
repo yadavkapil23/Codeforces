dca756a931b31d22f9648fb511fc63087066924e
Kapil <kydv901@gmail.com>
2026-02-20 19:39:53 +0530

    Create 405A-Gravity-Flip.cpp

---
+++ 405A-Gravity-Flip.cpp
@@ -0,0 +1,23 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    
+    vector<int> v;
+    int a;
+    for(int i=0 ; i<n ; i++){
+        cin>>a;
+        v.push_back(a);
+    }
+    
+    sort(v.begin(),v.end());
+    
+    for(int i=0 ; i<n ; i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
