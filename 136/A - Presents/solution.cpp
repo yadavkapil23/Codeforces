ccdd8ebcdf26e9556a3e1a8b762f8ad4b41ade4f
Kapil <kydv901@gmail.com>
2026-07-02 00:44:23 +0530

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    int p[100];
+    int ans[100];
+    
+    for(int i=1 ; i<=n ; i++){
+        cin>>p[i];
+        ans[p[i]] = i;
+    }
+    
+    for(int i=1 ; i<=n ; i++){
+        cout<<ans[i]<<endl;
+    }
+return 0;
+}
